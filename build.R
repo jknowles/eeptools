4b5037a61ee4746be7cf7d35e6d973b6c66bbb59
Jared Knowles <jknowles@gmail.com>
2012-07-28 20:11:21 -0500

    Fixed up the documentation so package builds without errors

--- build.R
+++ build.R
@@ -6,4 +6,6 @@ source('statamode.R')
 source('cutandthresh.R')
 
 #package.skeleton(name='eeptools')
+library(devtools)
+setwd('C:/Users/Jared/Projects')
 build(pkg=paste(getwd(),"/eeptools",sep=""),binary=TRUE)
