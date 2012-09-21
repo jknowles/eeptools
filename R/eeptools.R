bb1bf19b5cdb1793ae2b2eac56022fccb492a724
Jared Knowles <jknowles@gmail.com>
2012-09-21 09:16:41 -0500

    More R CMD check tweaks

--- R/eeptools.R
+++ R/eeptools.R
@@ -1,8 +1,9 @@
-.onAttach<-function(lib,pkg){
-  cat( c( " eeptools for R version 0.1",
+.onAttach<-function(...){
+  if (!interactive() || stats::runif(1) > 0.1) return()
+  tip<-cat(c(" eeptools for R version 0.1",
           " Free and Open Software for Education Evaluation",
           "Developed by Jared E. Knowles 2012",
-          " For The Wisconsin Department of Public Instruction",
+          "for The Wisconsin Department of Public Instruction",
           " Distributed without warranty"), sep="\n")
-  
+  packageStartupMessage(tip)
 }
