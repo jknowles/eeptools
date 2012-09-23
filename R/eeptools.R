9768a445ec825609e0eb2189199b2c13302d5029
Jared Knowles <jknowles@gmail.com>
2012-09-23 17:50:04 -0500

    Clean up dir Clean up doco

--- R/eeptools.R
+++ R/eeptools.R
@@ -1,9 +1,8 @@
 .onAttach<-function(...){
-  if (!interactive() || stats::runif(1) > 0.1) return()
-  tip<-cat(c(" eeptools for R version 0.1",
-          " Free and Open Software for Education Evaluation",
-          "Developed by Jared E. Knowles 2012",
-          "for The Wisconsin Department of Public Instruction",
-          " Distributed without warranty"), sep="\n")
-  packageStartupMessage(tip)
+  if (!interactive() ) return()
+  packageStartupMessage(paste(c(" eeptools for R version 0.1",
+                                " Free and Open Software for Education Evaluation",
+                                "Developed by Jared E. Knowles 2012",
+                                "for The Wisconsin Department of Public Instruction",
+                                " Distributed without warranty"), sep="\n"))
 }
