6b6e65f5d2754dd54df1f84ed548e3bfed9bb1fe
Jared Knowles <jknowles@gmail.com>
2012-10-03 10:05:51 -0500

    add options to statamode

--- R/statamode.R
+++ R/statamode.R
@@ -1,9 +1,26 @@
-statamode <-
-function(x) {
+statamode <- function(x,method=c("last", "stata","sample")){
+  if(method=='stata'){
   z <- table(as.vector(x))
   m<-names(z)[z == max(z)]
   if(length(m)==1){
     return(m)
   }
   return(".")
+  }
+  else if(method=='sample'){
+    z <- table(as.vector(x))
+    m<-names(z)[z == max(z)]
+    if(length(m)==1){
+      return(m)
+    }
+    return(sample(m,1))
+  }
+  else if(method=='last'){
+    z <- table(as.vector(x))
+    m<-names(z)[z == max(z)]
+    if(length(m)==1){
+      return(m)
+    }
+    return(tail(x,1))
+  }
 }
