354d3160686ea88e73c52abd2c49e0a98298c5c1
Jared Knowles <jknowles@gmail.com>
2013-03-27 11:34:18 -0500

    Fix package startup message

--- R/eeptools.R
+++ R/eeptools.R
@@ -1,8 +1,20 @@
 .onAttach<-function(...){
   if (!interactive() ) return()
-  packageStartupMessage(paste(c(" eeptools for R version 0.1",
-                                " Free and Open Software for Education Evaluation",
-                                "Developed by Jared E. Knowles 2012",
-                                "for The Wisconsin Department of Public Instruction",
-                                " Distributed without warranty"), sep="\n"))
+packageStartupMessage("Welcome to eeptools for R version 0.1 ",appendLF=TRUE)
+packageStartupMessage("Free and Open Software for Education Evaluation", appendLF=TRUE)
+packageStartupMessage("Developed by Jared E. Knowles 2012-2013", appendLF=TRUE)
+packageStartupMessage("for The Wisconsin Department of Public Instruction", appendLF=TRUE)
+packageStartupMessage("Distributed without warranty", appendLF=TRUE)
+
 }
+
+# testit <- function() {
+#   message("testing")
+#   packageStartupMessage("Welcome to eeptools for R version 0.1 ",appendLF=TRUE)
+#   packageStartupMessage("Free and Open Software for Education Evaluation", appendLF=TRUE)
+#   packageStartupMessage("Developed by Jared E. Knowles 2012-13", appendLF=TRUE)
+#   packageStartupMessage("for The Wisconsin Department of Public Instruction", appendLF=TRUE)
+#   packageStartupMessage("Distributed without warranty", appendLF=TRUE)
+#   Sys.sleep(1)
+#   packageStartupMessage("....")
+# }
