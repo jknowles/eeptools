8a92fcf9f4986888cabf6ab592a70a5583a05bca
Jared Knowles <jknowles@gmail.com>
2013-12-04 11:35:31 -0600

    Debugging and testing gelmansim

--- R/modsims.R
+++ R/modsims.R
@@ -8,21 +8,28 @@
 ##' @param na.omit Logical indicating whether to remove NAs from \code{newdata}
 ##' @return A dataframe with newdata and prediction intervals
 ##' @references Modified from Gelman and Hill 2006. Data Analysis Using Regression and Multilevel/Hierarchical Models. Cambridge University Press.
+##' @details Currently gelmansim does not work for \code{\link{lm}} objects because of the way \code{\link{sim}} in the 
+##' \code{arm} package handles variable names for these objects. It is recommended users use \code{\link{glm}} in these cases.
 ##' @export
 ##' @examples
 ##' \dontrun{
 ##'  
 ##' }
 gelmansim <- function(mod, newdata, nsims, na.omit=TRUE){
-  sims.tmp <- sim(mod, n.sims=nsims)
-  form.tmp <- mod$formula[-2]
+    sims.tmp <- sim(mod, n.sims=nsims)
+  if("lm" %in% class(mod)[1]){
+    form.tmp <- as.formula(paste("~",as.character(formula(mod)[3])))
+  } else{
+    form.tmp <- mod$formula[-2]
+  }
+ 
   
   # generate factors matrix from model
   not.numeric <- function(x) ifelse(is.numeric(x), FALSE, TRUE)
   
   # Get factors
   facs <- mod$model[,sapply(mod$model, not.numeric)]
-  
+  if(exists('facs')){
   if(class(facs) == "data.frame"){
     contr.tmp <- lapply(facs, unique)
     for(i in names(contr.tmp)){
@@ -33,7 +40,7 @@ gelmansim <- function(mod, newdata, nsims, na.omit=TRUE){
                                                      contrasts, contrasts=FALSE))
     }
   } else {
-    contr.tmp <- unique(facs)
+    contr.tmp <- levels(facs)
     names.tmp <- sapply(mod$model, not.numeric)
     facname <- names(names.tmp[names.tmp==TRUE])
     newdata[, facname] <- factor(newdata[, facname], 
@@ -44,15 +51,17 @@ gelmansim <- function(mod, newdata, nsims, na.omit=TRUE){
     X.tilde <- model.matrix(form.tmp, newdata, xlev=contr.tmp, 
                             contrasts.arg = contr.list)
   }
+  } else {
+    X.tilde <- model.matrix(form.tmp, newdata)
+  }
   
   if(na.omit==TRUE){
     newdata <- na.omit(newdata)
   }
   
   pred.tmp <- unlist(dimnames(sims.tmp@coef)[2])
-  X.tilde <- X.tilde[, colnames(X.tilde) %in% pred.tmp]
+  X.tilde <- X.tilde[, unlist(colnames(X.tilde)) %in% pred.tmp]
   X.tilde <- reorder(X.tilde, dim=2, names=pred.tmp)
-  
   n.tilde <- nrow(X.tilde)
   y.tilde <- array(NA, c(nsims, n.tilde))
   
