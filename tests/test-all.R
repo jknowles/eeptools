115c1353b6b5cabae2d915c3901425e1c5e12f7e
Jared E Knowles <jknowles@gmail.com>
2013-12-07 15:58:15 -0600

    Clean up documentation

--- tests/test-all.R
+++ tests/test-all.R
@@ -2,10 +2,10 @@ library(testthat)
 library(eeptools)
 
 test_package("eeptools")
-
-source("R/statamode.R")
-test_file("tests/testthat/test-statamode.R")
-source("R/decomma.R")
-test_file("tests/testthat/test-decomma.R")
-source("R/modsims.R")
-test_file("tests/testthat/test-gelmansim.R")
+# 
+# source("R/statamode.R")
+# test_file("tests/testthat/test-statamode.R")
+# source("R/decomma.R")
+# test_file("tests/testthat/test-decomma.R")
+# source("R/modsims.R")
+# test_file("tests/testthat/test-gelmansim.R")
