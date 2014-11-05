e408a064c04056866cb6ccc6e7bb057a33e391c2
Jared Knowles <jknowles@gmail.com>
2014-11-05 13:59:52 -0600

    fix up tests

--- tests/test-all.R
+++ tests/test-all.R
@@ -2,10 +2,3 @@ library(testthat)
 library(eeptools)
 
 test_check("eeptools")
-# 
-# source("R/statamode.R")
-# test_file("tests/testthat/test-statamode.R")
-# source("R/decomma.R")
-# test_file("tests/testthat/test-decomma.R")
-# source("R/modsims.R")
-# test_file("tests/testthat/test-gelmansim.R")
