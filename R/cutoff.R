972ae1c13c7def033a3ee4d7ee9a113a0ee147b6
Jared Knowles <jknowles@gmail.com>
2014-11-05 16:10:57 -0600

    roxygenize the de- functions

--- R/cutoff.R
+++ R/cutoff.R
@@ -1,8 +1,25 @@
-cutoff <-
-function(x,cutoff){
-  x<-x[order(-x)]               #sort vector descending
-  xb<-cumsum(x)                 # take cumulative sum
-  xc<-xb/sum(x,na.rm=T)        #express proportionally
-  length(xc[xc<cutoff])         #count number of items until 
-  #threshhold is exceeded
+##' A function to calculate thresholds in a vector.
+##' @description This function tells us how far we have to
+##' go before reaching a cutoff in a variable
+##' by sorting the vector, then finding how far 
+##' to go. Note that the cutoff is expressed in 
+##' percentage terms (fixed cumulative sum)
+##' @param x a numeric vector, missing values are allowed
+##' @param cutoff a user defined numeric value to stop the cutoff specified as 
+##' a proportion 0 to 1
+##' @details Calculates the distance through a numeric vector before a certain 
+##' proportion is reached by sorting the vector and calculating the cumulative proportion of each element
+##' @return An integer for the minimum number of elements necessary to reach cutoff
+##' @author Jared E. Knowles
+##' @export
+##' @examples
+##' # for vector
+##' a <- rnorm(100, mean=6, sd=1)
+##' cutoff(a, .7) #return minimum number of elements to account 70 percent of total
+##' 
+cutoff <- function(x,cutoff){
+  x <- x[order(-x)]
+  xb <- cumsum(x)
+  xc <- xb/sum(x, na.rm=T)
+  length(xc[xc < cutoff])
 }
