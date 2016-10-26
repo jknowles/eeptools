cf4f1f73ec7a5b93a8932098daa68d1477064ce8
Jared Knowles <jknowles@gmail.com>
2016-10-26 10:30:29 -0400

    remove deprecated funcs from examples

--- R/theme_dpi.R
+++ R/theme_dpi.R
@@ -1,6 +1,7 @@
-#' a ggplot2 theme developed for PDF and PNG for use at the Wisconsin Department of Public Instruction
+#' a deprecated ggplot2 theme developed for PDF and PNG for use at the 
+#' Wisconsin Department of Public Instruction
 #' @description This is a custom ggplot2 theme developed for the Wisconsin 
-#' Department of Public Instruction
+#' Department of Public Instruction. This function is now deprecated.
 #' @param base_size  numeric, specify the font size as a numeric value, default is 16
 #' @param base_family character, specify the font family, this value is optional
 #' @details All values are optional
@@ -11,18 +12,14 @@
 #' @author Jared E. Knowles
 #' @import ggplot2
 #' @export
-#'
-#' @examples
-#' qplot(mpg, wt, data=mtcars) # standard
-#' qplot(mpg, wt, data=mtcars) + theme_dpi()
 theme_dpi <- function (base_size = 16, base_family = "") {
   .Deprecated("theme_bw")
   theme_bw(base_size = base_size, base_family = base_family)
   
   }
 
-#' a ggplot2 theme developed for PDF or SVG maps
-#' @description This is a custom ggplot2 theme developed for the Wisconsin 
+#' a deprecated ggplot2 theme developed for PDF or SVG maps
+#' @description This is a deprecated ggplot2 theme developed for the Wisconsin 
 #' Department of Public Instruction for making PDF maps
 #' @param base_size numeric, specify the font size, default is 14
 #' @param base_family character, specify the font family, this value is optional
@@ -35,7 +32,6 @@ theme_dpi <- function (base_size = 16, base_family = "") {
 #' @author Jared E. Knowles
 #' @import ggplot2
 #' @export
-#'
 #' @examples
 #' # Data
 #' crimes <- data.frame(state = tolower(rownames(USArrests)), USArrests)
@@ -49,15 +45,15 @@ theme_dpi <- function (base_size = 16, base_family = "") {
 #' last_plot() + coord_map()
 #' # DPI theme
 #' ggplot(crimesm, aes(map_id = state)) + geom_map(aes(fill = value), map = states_map) + 
-#'     expand_limits(x = states_map$long, y = states_map$lat) + facet_wrap( ~ variable)+theme_dpi_map()
+#'     expand_limits(x = states_map$long, y = states_map$lat) + facet_wrap( ~ variable)+theme_bw()
 theme_dpi_map <- function(base_size = 14, base_family = "") {
   .Deprecated("theme_bw")
   theme_bw(base_size = base_size, base_family = base_family)
 }
 
 
-#' an alternate ggplot2 theme developed for PDF or SVG maps
-#' @description This is a custom ggplot2 theme developed for the Wisconsin 
+#' an alternate deprecated ggplot2 theme developed for PDF or SVG maps
+#' @description This is a deprecated ggplot2 theme developed for the Wisconsin 
 #' Department of Public Instruction for making PDF maps
 #' @param base_size numeric, specify the font size, default is 14
 #' @param base_family character, specify the font family, this value is optional
@@ -75,24 +71,23 @@ theme_dpi_map <- function(base_size = 14, base_family = "") {
 #' crimes <- data.frame(state = tolower(rownames(USArrests)), USArrests)
 #' require(reshape) # for melt
 #' crimesm <- melt(crimes, id = 1)
-#' # No DPI theme
+#' # No BW theme
 #' states_map <- map_data("state")
 #' ggplot(crimes, aes(map_id = state)) + geom_map(aes(fill = Murder), map = states_map) + 
 #'     expand_limits(x = states_map$long, y = states_map$lat)+ labs(title="USA Crime")
 #' # Draw map
 #' last_plot() + coord_map()
-#' # DPI theme
 #' ggplot(crimesm, aes(map_id = state)) + geom_map(aes(fill = value), map = states_map) + 
 #'     expand_limits(x = states_map$long, y = states_map$lat) + 
-#'     facet_wrap( ~ variable) + theme_dpi_map2()
+#'     facet_wrap( ~ variable) + theme_bw()
 theme_dpi_map2 <- function(base_size = 14, base_family = "") {
   .Deprecated("theme_bw")
   theme_bw(base_size = base_size, base_family = base_family)
 }
 
 
-#' an alternate ggplot2 theme developed for PNG or JPG maps
-#' @description This is a custom ggplot2 theme developed for the Wisconsin 
+#' an deprecated ggplot2 theme developed for PNG or JPG maps
+#' @description This is a deprecated ggplot2 theme developed for the Wisconsin 
 #' Department of Public Instruction for making PNG or JPG maps
 #' @param base_size numeric, specify the font size, default is 18
 #' @param base_family character, specify the font family, this value is optional
@@ -119,7 +114,7 @@ theme_dpi_map2 <- function(base_size = 14, base_family = "") {
 #' # DPI theme
 #' ggplot(crimesm, aes(map_id = state)) + geom_map(aes(fill = value), map = states_map) + 
 #'     expand_limits(x = states_map$long, y = states_map$lat) + 
-#'     facet_wrap( ~ variable) + theme_dpi_mapPNG()
+#'     facet_wrap( ~ variable) + theme_bw()
 theme_dpi_mapPNG<-function (base_size = 18, base_family = "") {
   .Deprecated("theme_bw")
   theme_bw(base_size = base_size, base_family = base_family)
