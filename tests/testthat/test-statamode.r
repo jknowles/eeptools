d79db8cbd96005535bc05cb82ba7fec196abfcf9
Jared Knowles <jknowles@gmail.com>
2018-01-26 09:26:19 -0500

    fix tests, fix build?

--- tests/testthat/test-statamode.r
+++ tests/testthat/test-statamode.r
@@ -1,10 +1,10 @@
 context("Correct mode selected")
 
 test_that("statamode selects the mode right for each method", {
-  expect_that(statamode("a"), matches("a"))
-  expect_that(statamode(c("a", "a", "b", "b"), method="stata"), matches("."))
-  expect_that(statamode(c("a", "a", "b", "b"), method="last"), matches("b"))
- # expect_that(statamode(c("a", "a", "b", "b"), method="sample"), matches(c("a", "b"),all=FALSE))
+  expect_that(statamode("a"), expect_match("a"))
+  expect_that(statamode(c("a", "a", "b", "b"), method="stata"), expect_match("."))
+  expect_that(statamode(c("a", "a", "b", "b"), method="last"), expect_match("b"))
+ # expect_that(statamode(c("a", "a", "b", "b"), method="sample"), expect_match(c("a", "b"),all=FALSE))
 })
 
 set.seed(100)
