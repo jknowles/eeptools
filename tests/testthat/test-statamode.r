c92b76a2c8ef0df3d82b3d44bb20febb47a8624c
Jared Knowles <jknowles@gmail.com>
2019-03-14 17:14:07 -0400

    RNG seeds

--- tests/testthat/test-statamode.r
+++ tests/testthat/test-statamode.r
@@ -8,14 +8,15 @@ test_that("statamode selects the mode right for each method", {
  # expect_that(statamode(c("a", "a", "b", "b"), method="sample"), matches(c("a", "b"),all=FALSE))
 })
 
-set.seed(100)
-a <- c(3, 1, 9, 8, 4, 4, 7, 7)
-b <- statamode(a, method = "last")
-c <- statamode(a, method = "stata")
-d <- statamode(a, method = "sample")
-
 
 test_that("statamode returns correct modes for each method using numbers", {
+  suppressWarnings(RNGversion("3.5.0"))
+  set.seed(100)
+  a <- c(3, 1, 9, 8, 4, 4, 7, 7)
+  b <- statamode(a, method = "last")
+  c <- statamode(a, method = "stata")
+  d <- statamode(a, method = "sample")
+  
   expect_is(b, "numeric")
   expect_is(c, "character")
   expect_is(d, "numeric")
