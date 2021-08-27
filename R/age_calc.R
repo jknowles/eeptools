447192d907644959526ecb0e389992059e2543f8
Benjamin <benjamin.nutter@gmail.com>
2021-08-27 09:44:54 -0400

    accommodate missing `dob` values
    I also took some liberties to streamline and remove some nested ifelse statement.

--- R/age_calc.R
+++ R/age_calc.R
@@ -1,5 +1,5 @@
 ##' Function to calculate age from date of birth.
-##' @description his function calculates age in days, months, or years from a 
+##' @description This function calculates age in days, months, or years from a 
 ##' date of birth to another arbitrary date. This returns a numeric vector in 
 ##' the specified units.
 ##' @param dob a vector of class \code{Date} representing the date of birth/start date
@@ -26,72 +26,122 @@
 ##' age <- age_calc(a, as.Date('2005-09-01'))
 ##' age
 age_calc <- function(dob, enddate=Sys.Date(), units='months', precise=TRUE){
+  units = match.arg(units, 
+                    c('days', 'months', 'years'))
+  
+  if (!is.logical(precise) & length(precise != 1)){
+    stop("`precise` must be a logical of length 1.")
+  }
+  
+  retval <- numeric(length(dob))
+  
+  is_dob_miss <- is.na(dob)
+  
+  retval[is_dob_miss] <- NA_real_
+  
+  
+  retval[!is_dob_miss] <- 
+    .age_calc_nomiss(dob[!is_dob_miss], 
+                         enddate = enddate, 
+                         units = units, 
+                         precise = precise)
+  
+  retval
+}
+
+.age_calc_nomiss <- function(dob, enddate=Sys.Date(), units='months', precise=TRUE){
   if (!inherits(dob, "Date") | !inherits(enddate, "Date")) {
     stop("Both dob and enddate must be Date class objects")
   }
-  if (any(enddate < dob)) {
+  
+  if (any(vapply(enddate < dob, isTRUE, logical(1)))) {
     stop("End date must be a date after date of birth")
   }
+  
   start <- as.POSIXlt(dob)
   end <- as.POSIXlt(enddate)
   if (precise) {
-    start_is_leap <- ifelse(start$year %% 400 == 0, TRUE, 
-                            ifelse(start$year %% 100 == 0, FALSE,
-                                   ifelse(start$year %% 4 == 0, TRUE, FALSE)))
-    end_is_leap <- ifelse(end$year %% 400 == 0, TRUE, 
-                          ifelse(end$year %% 100 == 0, FALSE,
-                                 ifelse(end$year %% 4 == 0, TRUE, FALSE)))
+    start_is_leap <- .is_leap_year(start$year)
+    end_is_leap <- .is_leap_year(end$year)
   }
-  if (units == 'days') {
-    result <- difftime(end, start, units = 'days')
-  } else if (units == 'months') {
-    months <- vapply(mapply(seq, as.POSIXct(start), as.POSIXct(end), 
-                            by = 'months', SIMPLIFY = FALSE), 
-                     length, FUN.VALUE = length(start)) - 1
-    # length(seq(start, end, by='month')) - 1
-    if (precise) {
-      month_length_end <- ifelse(end$mon == 1 & end_is_leap, 29,
-                                 ifelse(end$mon == 1, 28, 
-                                        ifelse(end$mon %in% c(3, 5, 8, 10), 
-                                               30, 31)))
-      month_length_prior <- ifelse((end$mon - 1) == 1 & start_is_leap, 29, 
-                                   ifelse((end$mon - 1) == 1, 28, 
-                                        ifelse((end$mon - 1) %in% c(3, 5, 8, 10), 
-                                                 30, 31)))
-      month_frac <- ifelse(end$mday > start$mday,
-                           (end$mday - start$mday) / month_length_end,
-                           ifelse(end$mday < start$mday, 
-                                  (month_length_prior - start$mday) / 
-                                    month_length_prior + 
-                                    end$mday/month_length_end, 0.0))
-      result <- months + month_frac
-    }else{
-      result <- months
-    }
-  } else if (units == 'years') {
-    years <- vapply(mapply(seq, as.POSIXct(start), as.POSIXct(end), 
-                           by = 'years', SIMPLIFY = FALSE), 
-                    length, FUN.VALUE = length(start)) - 1
-    if (precise) {
-      start_length <- ifelse(start_is_leap, 366, 365)
-      end_length <- ifelse(end_is_leap, 366, 365)
-      start_day <- ifelse(start_is_leap & start$yday >= 60,
-                          start$yday - 1,
-                          start$yday)
-      end_day <- ifelse(end_is_leap & end$yday >= 60,
-                        end$yday - 1,
-                        end$yday)
-      year_frac <- ifelse(start_day < end_day,
-                          (end_day - start_day)/end_length,
-                          ifelse(start_day > end_day, 
-                                 (start_length - start_day) / start_length +
-                                   end_day / end_length, 0.0))
-      result <- years + year_frac
-    }else{
-      result <- years
-    }
-  }else{
+  
+  switch(
+    units, 
+    'days' = 
+      {
+        difftime(end, start, units = 'days')
+      }, 
+    'months' = 
+      {
+        months <- mapply(seq, as.POSIXct(start), 
+                         as.POSIXct(end), 
+                         MoreArgs = list(by = 'months'), 
+                         SIMPLIFY = FALSE)
+        months <- vapply(months, 
+                         length, 
+                         numeric(1)) - 1
+        
+        if (precise) {
+          month_length_end <- .day_in_month(end$mon, 
+                                            end_is_leap)
+          month_length_prior <- .day_in_month(end$mon, 
+                                              start_is_leap)
+          
+          eg <- end$mday > start$mday
+          el <- end$mday < start$mday
+          month_frac <- rep(0.0, length(end$mday))
+          month_frac[eg] <- (end$mday[eg] - start$mday[eg]) / month_length_end[eg]
+          month_frac[el] <- (month_length_prior[el] - start$mday[el]) / month_length_prior[el] + end$mday[el]/month_length_end[el]
+          
+          months + month_frac
+        }else{
+          months
+        }
+      }, 
+    'years' = 
+      {
+        years <- mapply(seq, 
+                        as.POSIXct(start), 
+                        as.POSIXct(end), 
+                        MoreArgs = list('years'), 
+                        SIMPLIFY = FALSE)
+        years <- vapply(years, 
+                        length, 
+                        numeric(1)) - 1
+        if (precise) {
+          start_length <- 365 + start_is_leap
+          end_length <- 365 + end_is_leap 
+          
+          start_day <- start$yday - (start_is_leap & start$yday >= 60)
+          end_day <- end$yday - (end_is_leap & end$yday >= 60)
+          
+          sl <- start_day < end_day
+          sg <- start_day > end_day
+          year_frac <- rep(0.0, start_day)
+          year_frac[sl] <- (end_day[sl] - start_day[sl])/end_length[sl]
+          year_frac[sg] <- (start_length[sg] - start_day[sg]) / start_length[sg] + end_day[sg] / end_length[sg]
+          
+          years + year_frac
+        }else{
+          years
+        }
+      }, 
     stop("Unrecognized units. Please choose years, months, or days.")
-  }
-  return(result)
+  )
+}
+
+.is_leap_year <- function(year){
+  year %% 400 == 0 &
+    year %% 100 != 0 &
+    year %% 4 == 0
+}
+
+.day_in_month <- function(month, is_leap){
+  retval <- rep(31, length(month))
+  
+  retval[month == 1 & is_leap] <- 29
+  retval[month == 1 & !is_leap] <- 28
+  retval[month %in% c(3, 5, 8, 10)] <- 30
+  
+  retval
 }
