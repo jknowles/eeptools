63cf89db618976ac8da8a39211ee9d1c1d522062
= <jknowles@gmail.com>
2023-08-23 10:03:02 -0400

    reorg package doco

--- R/eeptools-package.r
+++ R/eeptools-package.r
@@ -2,14 +2,8 @@ utils::globalVariables(c("adv", "grade", "count", "id", "gradeP", "vals", "prof"
 #' Evaluation of educational policy tools
 #' @description Make common tasks for educational evaluation easier to do!
 #' @name eeptools
-#' @details \tabular{ll}{
-#' Package: \tab eeptools\cr
-#' Type: \tab Package\cr
-#' Version: \tab 1.2.0 \cr
-#' Date: \tab 2018-06-01\cr
-#' License: \tab GPL-3 \cr
-#' }
-#' his package has a number of useful shortcuts for common tasks. It includes 
+#' @details
+#' This package has a number of useful shortcuts for common tasks. It includes 
 #' some themes for ggplot2 plots, processing arbitrary text files of data, 
 #' calculating student characteristics, and finding thresholds within vectors. 
 #' Future development work will include methods for tuning and evaluating early 
@@ -17,6 +11,7 @@ utils::globalVariables(c("adv", "grade", "count", "id", "gradeP", "vals", "prof"
 #' @author Jared E. Knowles
 #' @note This package is still in beta and function names may change in the next release.
 #' @docType package
+#' @keywords "_PACKAGE"
 #' @examples 
 #' gender<-c("M","M","M","F","F","F")
 #' statamode(gender)
